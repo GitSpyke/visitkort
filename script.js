6732e63f1cbde0c5c968af749e897550678d37c3
GitSpyke <36282472+GitSpyke@users.noreply.github.com>
2020-08-25 00:05:01 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -10,68 +10,67 @@ firebase.initializeApp({
   measurementId: "G-997NY58CV0"
 });
 
-const orderArray = ["name", "surName", "telephone", "email", "image"];
-
-var collectionReference = firebase.firestore().collection("visitkort");
-var queryToGetHighestID = collectionReference.orderBy("id", "desc").limit(1);
-
-let idCount;
+// is used mainly as a reference for the order between fields
+const orderArray = ["name", "surName", "telephone", "email"];
+// reference to the image storage (storageReference)
+var storRef = firebase.storage().ref();
+// reference to the database (collectionReference)
+var collRef = firebase.firestore().collection("visitkort");
+// variable to keep track of ID incrementation
+var idCount = 0;
+// ID variable used for modification and updating of related card
+var toModifyID;
 
 // gets the highest ID of all items in the database
-queryToGetHighestID.get().then(function (querySnapshot) {
+collRef.orderBy("id", "desc").limit(1).get().then(function (querySnapshot) {
   querySnapshot.forEach(function (doc) {
     idCount = doc.id;
   })
 })
 
 // pushes values to the database
-function addValues(id, imageURL) {
-  console.log(id);
-  collectionReference.doc(String(id)).set({
-
+function addValues(id) {
+  collRef.doc(String(id)).set({
     id: id,
     name: form.name.value,
     surName: form.surName.value,
     telephone: form.telephone.value,
     email: form.email.value,
-    image: imageURL
+    image: id + "image"
   })
 }
 
+// creates and adds table representing a card onto the interface
 function createCardGraphic(data) {
   var table = document.createElement('table');
-  table.id =  + data.id + " table";
-  table.className = "card";
-  table.style.width = '350px';
-  table.style.border = '1px solid black';
   var td = table.insertRow().insertCell();
-  td.style.borderBottom = '1px solid black';
-  td.style.textAlign = 'center';
+  var imgtd = table.insertRow().insertCell();
+  table.className = "card";
+  td.className = "cardHeader";
   td.appendChild(document.createTextNode("Visitkort"));
-  for (temp in orderArray) {
+  storRef.child(data["image"]).getDownloadURL().then(function(url) {
+    imgtd.innerHTML = "<img src=" + url + " height='40px' />";
+  });
+  for (element in orderArray) {
+    var field = orderArray[element];
     var td = table.insertRow().insertCell();
-    var field = orderArray[temp];
-    td.appendChild(document.createTextNode(orderArray[temp] + ": " + data[orderArray[temp]]+ "\n"));
+    var field = orderArray[element];
+    td.appendChild(document.createTextNode(field + ": " + data[field]+ "\n"));
     }
-  document.body.appendChild(table);
+  $(".right").append(table);
 }
 
 // adds a button for modifying a specific card
 function addButton(type, data) {
-  var modifyButton = document.createElement('button');
-  modifyButton.className = type + " button";
-  modifyButton.id = data.id + "_" + type + "Button";
-  modifyButton.style.height = '27px';
-  modifyButton.style.margin = '5px';
-  if (type == "modify") {
-    modifyButton.innerHTML = "Ändra ovanstående visitkort";
-  } else if (type == "remove") {
-    modifyButton.innerHTML = "Ta bort ovanstående visitkort";
-  }
-  document.body.appendChild(modifyButton);
+  key = {"modify": "Ändra ", "remove": "Ta bort "}
+  var tempButton = document.createElement('button');
+  tempButton.className = type + " button";
+  tempButton.id = data.id + "_" + type + "Button";
+  tempButton.innerHTML = key[type] + "ovanstående visitkort";
+  $(".right").append(tempButton);
 }
 
-// removes currently listed cards and associated buttons
+// removes currently listed cards and associated buttons from interface
 function removeListOfCards() {
   $(".modify").remove();
   $(".card").remove();
@@ -81,23 +80,20 @@ function removeListOfCards() {
 // lists all cards in the database
 function listCards() {
   removeListOfCards();
-  collectionReference.get().then(function(querySnapshot) {
+  collRef.orderBy("id", "desc").get().then(function(querySnapshot) {
     querySnapshot.forEach(function(doc) {
       createCardGraphic(doc.data());
       addButton("modify", doc.data());
       addButton("remove", doc.data());
     })
-  });
+  })
 }
-
-let imageURL;
 
 // encodes image as Base64
 function encodeImageAsURL(image) {
   var file = image.files[0];
   var reader = new FileReader();
   reader.onloadend = function() {
-    console.log('RESULT', reader.result)
     imageURL = reader.result;
   }
   reader.readAsDataURL(file);
@@ -105,41 +101,51 @@ function encodeImageAsURL(image) {
 
 // adds a new document to the database with data from the form
 function addCard() {
-  if (isNaN(idCount)) {idCount = 0;}
-  idCount++;
-  var storageReference = firebase.storage().ref();
-  var imageReference = storageReference.child(idCount + "image");
-  var imageImagesReference = storageReference.child("images/count" + "image");
+  addValues(idCount++)
+  var imageReference = storRef.child(idCount + "image");
   imageReference.putString(imageURL, 'data_url').then(function(snapshot) {
-    console.log('Uploaded the image as a base64 string!');
-  })
-  addValues(idCount, "gs://visitkort-76fe8.appspot.com/" + idCount + "image")
+  listCards() //maybe just one
+  });
 };
 
-// is supposed to modify currenttly displayed card
+// updates currenttly accessed card
 function updateCard() {
+  $("#updateButton").prop('disabled', true);
+  var imageReference = storRef.child(idCount + "image");
+  imageReference.putString(imageURL, 'data_url');
+  collRef.doc(toModifyID).update({
+    name: form.name.value,
+    surName: form.surName.value,
+    telephone: form.telephone.value,
+    email: form.email.value,
+  }).then(function(snapshot) {
+  listCards() //maybe just one
+  })
+}
 
+// fills fields in form with data from provided document
+function fillFields(data) {
+  for (element in orderArray.slice(0,-1)) {
+    $("#" + orderArray[element]).val(data[orderArray[element]])
+  }
 }
 
 // removes document from database based on id
 $(document).on("click", ".remove", function(event) {
-  collectionReference.doc(this.id.split("_")[0]).delete();
+  collRef.doc(this.id.split("_")[0]).delete();
   listCards();
 });
 
-// is supposed to allow for modification of a document based on id
+// allows for modification of a document based on id
 $(document).on("click", ".modify", function(event) {
-  var id = (this.id).split("_")[0];
-  var documentReference = collectionReference.doc(id);
-  documentReference.get().then(function(doc) {
-    console.log($("#name").value)
-    $("#name").value = doc.data()["name"];
-    $("#surName").value = doc.data()["surName"];
-    $("#telephone").value = doc.data()["telephone"];
-    $("#email").value = doc.data()["email"];
-    //$("#modifyButton".disabled = false);
+  toModifyID = (this.id).split("_")[0];
+  collRef.doc((this.id).split("_")[0]).get().then(function(doc) {
+    fillFields(doc.data());
+    $("#updateButton").prop('disabled', false);
+    window.scrollTo({top: 0, behavior: 'smooth'});
   })
 })
 
+$("#updateButton").click(updateCard);
 $("#addButton").click(addCard);
 $("#getButton").click(listCards);
