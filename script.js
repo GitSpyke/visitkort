498dd6b7e5ab07b8e9cd57b6aa1bc018cb2a6629
GitSpyke <36282472+GitSpyke@users.noreply.github.com>
2020-08-26 04:03:56 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -10,31 +10,29 @@ firebase.initializeApp({
   measurementId: "G-997NY58CV0"
 });
 
-// is used mainly to maintain the order between fields
+// is used mainly as a reference for the order between fields
 const orderArray = ["name", "surName", "telephone", "email"];
-
-// reference to image storage
-var storageReference = firebase.storage().ref();
-//reference to the database
-var collectionReference = firebase.firestore().collection("visitkort");
-var queryToGetHighestID = collectionReference.orderBy("id", "desc").limit(1);
-
-// ID variable to keep track of incrementation
-let idCount = 0;
-
-// ID variable used for modification and updating of card
-let toModifyID;
+// reference to the image storage (storageReference)
+var storRef = firebase.storage().ref();
+// reference to the database (collectionReference)
+var collRef = firebase.firestore().collection("visitkort");
+// variable to keep track of ID incrementation
+var countID = 0;
+// ID variable used for modification and updating of related card
+var toModifyID;
+// varable that will represent any current image-endoding
+var imageString;
 
 // gets the highest ID of all items in the database
-queryToGetHighestID.get().then(function (querySnapshot) {
+collRef.orderBy("id", "desc").limit(1).get().then(function (querySnapshot) {
   querySnapshot.forEach(function (doc) {
-    idCount = doc.id;
+    countID = doc.id;
   })
 })
 
 // pushes values to the database
 function addValues(id) {
-  collectionReference.doc(String(id)).set({
+  collRef.doc(String(id)).set({
     id: id,
     name: form.name.value,
     surName: form.surName.value,
@@ -47,40 +45,34 @@ function addValues(id) {
 // creates and adds table representing a card onto the interface
 function createCardGraphic(data) {
   var table = document.createElement('table');
-  //table.id =  + data.id + "table";
-  table.className = "card";
   var td = table.insertRow().insertCell();
-  td.appendChild(document.createTextNode("Visitkort"));
-  td.className = "cardHeader";
   var imgtd = table.insertRow().insertCell();
-  storageReference.child(data["image"]).getDownloadURL().then(function(url) {
+  table.className = "card";
+  td.className = "cardHeader";
+  td.appendChild(document.createTextNode("Visitkort"));
+  storRef.child(data["image"]).getDownloadURL().then(function(url) {
     imgtd.innerHTML = "<img src=" + url + " height='40px' />";
-  // Get the download URL for 'images/stars.jpg'
-  // This can be inserted into an <img> tag
-  // This can also be downloaded directly
-});
+  });
   for (element in orderArray) {
+    var field = orderArray[element];
     var td = table.insertRow().insertCell();
     var field = orderArray[element];
-    td.appendChild(document.createTextNode(orderArray[element] + ": " + data[orderArray[element]]+ "\n"));
+    td.appendChild(document.createTextNode(field + ": " + data[field]+ "\n"));
     }
   $(".right").append(table);
 }
 
 // adds a button for modifying a specific card
 function addButton(type, data) {
+  key = {"modify": "Ändra ", "remove": "Ta bort "}
   var tempButton = document.createElement('button');
   tempButton.className = type + " button";
   tempButton.id = data.id + "_" + type + "Button";
-  if (type == "modify") {
-    tempButton.innerHTML = "Ändra ovanstående visitkort";
-  } else if (type == "remove") {
-    tempButton.innerHTML = "Ta bort ovanstående visitkort";
-  }
+  tempButton.innerHTML = key[type] + "ovanstående visitkort";
   $(".right").append(tempButton);
 }
 
-// removes currently listed cards and associated buttons
+// removes currently listed cards and associated buttons from interface
 function removeListOfCards() {
   $(".modify").remove();
   $(".card").remove();
@@ -90,13 +82,13 @@ function removeListOfCards() {
 // lists all cards in the database
 function listCards() {
   removeListOfCards();
-  collectionReference.orderBy("id", "desc").get().then(function(querySnapshot) {
+  collRef.orderBy("id", "desc").get().then(function(querySnapshot) {
     querySnapshot.forEach(function(doc) {
       createCardGraphic(doc.data());
       addButton("modify", doc.data());
       addButton("remove", doc.data());
     })
-  });
+  })
 }
 
 // encodes image as Base64
@@ -104,30 +96,36 @@ function encodeImageAsURL(image) {
   var file = image.files[0];
   var reader = new FileReader();
   reader.onloadend = function() {
-    imageURL = reader.result;
+    imageString = reader.result;
   }
   reader.readAsDataURL(file);
+};
+
+// uploads image as Base64 string to the image storage
+function uploadImage(id) {
+  var imgRef = storRef.child(id + "image");
+  imgRef.putString(imageString, 'data_url').then(function(snapshot) {
+  listCards() //maybe just one, also shouldn't be here if supposed to be dynamic
+  });
 }
 
 // adds a new document to the database with data from the form
 function addCard() {
-  idCount++;
-  addValues(idCount)
-  var imageReference = storageReference.child(idCount + "image");
-  imageReference.putString(imageURL, 'data_url').then();
-  listCards() //maybe just one
+  countID++;
+  addValues(countID);
+  uploadImage(countID)
 };
 
-// updates currenttly accessed card
+// updates currently accessed card
 function updateCard() {
   $("#updateButton").prop('disabled', true);
-  collectionReference.doc(toModifyID).update({
+  collRef.doc(toModifyID).update({
     name: form.name.value,
     surName: form.surName.value,
     telephone: form.telephone.value,
     email: form.email.value,
-  })
-  listCards();
+  });
+  uploadImage(toModifyID)
 }
 
 // fills fields in form with data from provided document
@@ -139,14 +137,14 @@ function fillFields(data) {
 
 // removes document from database based on id
 $(document).on("click", ".remove", function(event) {
-  collectionReference.doc(this.id.split("_")[0]).delete();
+  collRef.doc(this.id.split("_")[0]).delete();
   listCards();
 });
 
 // allows for modification of a document based on id
 $(document).on("click", ".modify", function(event) {
   toModifyID = (this.id).split("_")[0];
-  collectionReference.doc(toModifyID).get().then(function(doc) {
+  collRef.doc((this.id).split("_")[0]).get().then(function(doc) {
     fillFields(doc.data());
     $("#updateButton").prop('disabled', false);
     window.scrollTo({top: 0, behavior: 'smooth'});
